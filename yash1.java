34b59d61c271008013e50318867cdf3029c0e9b7
yash singare <112152093+Yashsingare@users.noreply.github.com>
2024-07-03 14:11:41 +0530

    Update yash1.java

--- yash1.java
+++ yash1.java
@@ -2,7 +2,7 @@ class Hello
   {
      public static void main(String args[])
     {
-     System.out.println("Hello world 33  etherhjetjhjhsaag");
+     System.out.println("Hello world 33  etherhjetjhjhsaa  wrghwqhgghbbhwswwbwabng");
 
     }
 
